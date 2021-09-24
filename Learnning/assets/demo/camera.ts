895e02b2b854f843d07cf5c66af7124485c73ae3
baodainguyen <baodaihd89@gmail.com>
2021-09-24 22:48:42 +0700

    learn rigidbody and shake camera

--- Learnning/assets/demo/camera.ts
+++ Learnning/assets/demo/camera.ts
@@ -10,6 +10,8 @@ export class Camera extends Component {
     private duration = 1.16;
 	public speed = 0.9;
 	public magnitude = 0.36;
+    
+    private scheduleTimes = 59.4827586207;
 
     // [2]
     // @property
@@ -20,55 +22,27 @@ export class Camera extends Component {
     }
     onKeyDown(event:any){
         switch(event.keyCode) {
-            case macro.KEY.r:
-                //this.initVal();
-               this.shake(0.016684999999999946);
+            case macro.KEY.z:
+               this.shakeSmall();
+                break;
+            case macro.KEY.x:
+                this.shakeLarge();
                 break;
         }
     }
-    start(){
-       this.originCamPos = this.node.getWorldPosition();
-       //this.elapsed = this.duration;
+
+    public shakeSmall(){
+        this.duration = 0.6;
+        this.magnitude = 0.12;
+        this.shake(0.016684999999999946);
     }
-    update (deltaTime: number) {
-        
+    public shakeLarge(){
+        this.duration = 1.16;
+        this.magnitude = 0.36;
+        this.shake(0.016684999999999946);
     }
-    updateShake(deltaTime:number){
-        if(this.elapsed < this.duration){
-            this.elapsed += deltaTime;			
-
-            let percentComplete = this.elapsed / this.duration;			
-
-        // We want to reduce the shake from full power to 0 starting half way through
-            let damper = 1.0 - clamp(2.0 * percentComplete - 1.0, 0.0, 1.0);
-
-        // Calculate the noise parameter starting randomly and going as fast as speed allows
-            let alpha = this.randomStart + this.speed * percentComplete;
-
-        // map noise to [-1, 1]
-            let x = this.noise(alpha, 0.0) * 2.0 - 1.0;
-            let y = this.noise(0.0, alpha) * 2.0 - 1.0;
-            
-            x *= this.magnitude * damper;
-            y *= this.magnitude * damper;
-
-            x = this.originCamPos.x + x;
-            y = this.originCamPos.y + y;
-
-            this.node.setWorldPosition(new Vec3(x, y, this.originCamPos.z));
-
-            console.log(this.node.getWorldPosition(), this.randomStart)
-        }
-    }
-    elapsed = 0.0;
-    originCamPos:Vec3;
-    randomStart = 0;
-    initVal(){
-        this.elapsed = 0.0;
-        this.randomStart = randomRange(-6.9, 6.9);
-        this.originCamPos = this.node.getWorldPosition();
-    }
-    shake(deltaTime:number) {   // 0.016684999999999946
+    
+    private shake(deltaTime:number) {   // 0.016684999999999946
         let elapsed = 0.0;
         let randomStart = randomRange(-6.9, 6.9);
         let originCamPos = this.node.getWorldPosition();
@@ -100,7 +74,7 @@ export class Camera extends Component {
             }
            // console.log(this.duration - elapsed);
             console.log(this.node.getPosition());
-        }, deltaTime, 69, 0);
+        }, deltaTime, this.scheduleTimes * this.duration, 0);   // scheduleTimes * 1.16 = 69
     }
     noise(x:number, y:number) { //http://stackoverflow.com/questions/16569660/2d-perlin-noise-in-c
         let n = x + y * 57;
